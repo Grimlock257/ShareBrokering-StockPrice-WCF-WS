9276358be38001de637138d91f41f926cf6037a7
Adam Watson <adamwatsonoa@gmail.com>
2020-12-01 10:04:34 +0000

    [SCCC-57] Create initital WCF project with ASMX Web Service

---
+++ StockPriceWebService/App_Code/StockPrice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Services;
+
+/// <summary>
+/// StockPrice Web Service
+/// </summary>
+[WebService(Namespace = "http://grimlock257.github.io/Stocks")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+public class StockPrice : WebService
+{
+    [WebMethod]
+    public string HelloWorld(String name)
+    {
+        return "Hello " + name;
+    }
+}
