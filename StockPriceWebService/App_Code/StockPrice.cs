2b16428e4ab6d13acd72a7f7a3eb5b096bd619ea
Adam Watson <adamwatsonoa@gmail.com>
2020-12-01 16:17:17 +0000

    [SCCC-58] Create initial web method implementation that returns entire yahoo response JSON object as a string

--- StockPriceWebService/App_Code/StockPrice.cs
+++ StockPriceWebService/App_Code/StockPrice.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.IO;
+using System.Net;
 using System.Web.Services;
 
 /// <summary>
@@ -9,8 +10,42 @@ using System.Web.Services;
 public class StockPrice : WebService
 {
     [WebMethod]
-    public string HelloWorld(String name)
+    public string GetStockPrice(string symbol)
     {
-        return "Hello " + name;
+        // If parameter is null, return an empty string
+        if (symbol == null) return "";
+
+        // Request the stock price from the API
+        var sharePrice = RequestSharePrice(symbol);
+
+        // Return the response string
+        return sharePrice;
+    }
+
+    /// <summary>
+    /// Request the share price for the supplied symbol from the yahoo API
+    /// </summary>
+    /// <param name="symbol">The symbol for which to search</param>
+    /// <returns>The current share price for the supplied symbol</returns>
+    public static string RequestSharePrice(string symbol)
+    {
+        // Construct the URL
+        var url = "https://query1.finance.yahoo.com/v8/finance/chart/" + symbol + "?interval=1d&range=1d";
+
+        // Create the WebRequest object to send to the URL
+        var req = WebRequest.Create(url);
+        req.ContentType = "application/json; charset=utf-8";
+        req.Method = "GET";
+
+        // Retrieve the response from the request, if null, return empty string
+        var responseSteam = req.GetResponse().GetResponseStream();
+
+        if (responseSteam == null) return "";
+
+        // Read the response stream and return a trimmer version
+        var sr = new StreamReader(responseSteam);
+
+        return sr.ReadToEnd().Trim();
+
     }
 }
